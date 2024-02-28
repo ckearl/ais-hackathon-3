ea4dda17c70b352b5490863a7b584338153a69ce
Braden Toone <braden@toonetown.com>
2024-02-28 12:57:23 -0700

    Added fetchEvents async func to Database class

--- public/js/firebase/database.js
+++ public/js/firebase/database.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from '/js/firebase/firebaseConfig.js';
+import { firebaseConfig } from './firebaseConfig.js';
 // import { initializeApp } from 'firebase/app';
 // import { collection, addDoc } from 'firebase/firestore';
 import { initializeApp } from 'https://cdn.skypack.dev/firebase/app';
@@ -21,7 +21,18 @@ class Database {
         throw error; // Rethrow the error so it can be handled elsewhere
       }
     }
-
+    async fetchEvents() {
+      try{
+        const docsSnapshot = await getDocs(collection(db, "events"));
+        const docs = [];
+        docsSnapshot.forEach(doc => {
+          docs.push(doc.data());
+        });
+        return docs;
+      } catch (error) {
+        throw error;
+      }
+    }
 }
 
 export { Database };
