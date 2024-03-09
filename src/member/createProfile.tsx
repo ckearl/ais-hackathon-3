384b3a1d95b4dcea5fc7cfc71bc6d7b39a0918f7
Joseph Fuge <josephfuge1@gmail.com>
2024-03-08 17:50:59 -0700

    export createProfile

--- src/member/createProfile.tsx
+++ src/member/createProfile.tsx
@@ -1,4 +1,4 @@
-function CreateProfile() {
+export function CreateProfile() {
   return (
     <form id="createAccount">
       <h2>Create Account</h2>
@@ -11,15 +11,45 @@ function CreateProfile() {
       <input type="text" id="netID" name="netID" required></input>
       <p>Student Type</p>
       <div className="radioSelector">
-        <input type="radio" id="junior" name="studentType" value="junior" required></input>
+        <input
+          type="radio"
+          id="junior"
+          name="studentType"
+          value="junior"
+          required
+        ></input>
         <label htmlFor="junior">IS Junior Core</label>
-        <input type="radio" id="senior" name="studentType" value="senior" required></input>
+        <input
+          type="radio"
+          id="senior"
+          name="studentType"
+          value="senior"
+          required
+        ></input>
         <label htmlFor="senior">IS Senior</label>
-        <input type="radio" id="mism" name="studentType" value="mism" required></input>
+        <input
+          type="radio"
+          id="mism"
+          name="studentType"
+          value="mism"
+          required
+        ></input>
         <label htmlFor="mism">MISM</label>
-        <input type="radio" id="other-byu" name="studentType" value="other-byu" required></input>
+        <input
+          type="radio"
+          id="other-byu"
+          name="studentType"
+          value="other-byu"
+          required
+        ></input>
         <label htmlFor="other-byu">BYU Student</label>
-        <input type="radio" id="other-guest" name="studentType" value="other-guest" required></input>
+        <input
+          type="radio"
+          id="other-guest"
+          name="studentType"
+          value="other-guest"
+          required
+        ></input>
         <label htmlFor="other-guest">Other/Guest</label>
       </div>
       <button type="submit">Create Account</button>
